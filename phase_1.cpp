4d6b94765b3dce3537b6e5b7da4fa867b65cfdae
alyciaa <156627970+alyciaaaaaaa@users.noreply.github.com>
2024-03-13 14:21:15 +0000

    update 9.21 pm

--- phase_1.cpp
+++ phase_1.cpp
@@ -3,5 +3,7 @@
 using namespace std;
 int main()
 {
-    cout<<"ginger\n"; //show text //c out form libraly std
+    cout<<"ginger"<<" namkhing\n"; //show text //c out form libraly std
+    cout<<"eieieiei";
+    cout<<endl; //same with \n //endl = end line
 }
