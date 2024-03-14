e5b4bb404cccd1c2b21b8fc442db6cc7594865e0
alyciaa <156627970+alyciaaaaaaa@users.noreply.github.com>
2024-03-14 16:11:15 +0000

    update 11.11 pm

--- phase_1.cpp
+++ phase_1.cpp
@@ -17,6 +17,17 @@ int main()
     //num1 = 100;
     //num2 = 200;
 
+    float score = 80.5;
+    string name = "nongtumm"; //use doublequote for string
+    int age=20;
+    bool pass = true; //bool from bollen true=1,false=0
+    char grade = 'A'; //use singlequote
+
     cout<<"number 1 is "<<num1<<"\n";
     cout<<"number 2 is "<<num2<<"\n";
+    cout<<"score = "<<score<<"\n";
+    cout<<"pass  = "<<pass<<"\n";
+    cout<<"grade = "<<grade<<"\n";
+    cout<<"student name  : "<<name<<"\n";
+    cout<<"age           : "<<age<<"\n";
 }
