0890728da7d2a8c01da7014c5fa9169fbfc139e2
alyciaa <156627970+alyciaaaaaaa@users.noreply.github.com>
2024-03-14 15:47:16 +0000

    update 10.47 pm

--- phase_1.cpp
+++ phase_1.cpp
@@ -1,9 +1,22 @@
 #include<iostream>
 
-using namespace std;
+using namespace std; //shirk it from std::cout to just cout
 int main()
 {
-    cout<<"ginger"<<" namkhing\n"; //show text //c out form libraly std
-    cout<<"eieieiei";
-    cout<<endl; //same with \n //endl = end line
+    //show text
+    cout<<"ginger"<<" namkhing\n";
+    //c out form libraly std
+    cout<<"eieieiei"<<"  :))";
+    cout<<endl; //same with \n 
+    //endl = end line
+
+
+    //variable
+    int num1 =100; //or int num1=100,num2=200;
+    int num2=200;
+    //num1 = 100;
+    //num2 = 200;
+
+    cout<<"number 1 is "<<num1<<"\n";
+    cout<<"number 2 is "<<num2<<"\n";
 }
